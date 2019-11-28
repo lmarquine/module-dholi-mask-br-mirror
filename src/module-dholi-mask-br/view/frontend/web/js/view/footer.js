189a2c388228d224a6b5540ccbedb545e71fc99b
eloom <contato@eloom.com.br>
2019-11-28 14:41:29 -0300

    Composer

--- src/module-dholi-mask-br/view/frontend/web/js/view/footer.js
+++ src/module-dholi-mask-br/view/frontend/web/js/view/footer.js
@@ -1,1 +1,1 @@
-define(["jquery","uiComponent","js/view/mask"],function(c,b,a){return b.extend({defaults:{},initialize:function(){this._super();a.apply()}})});
+define(["jquery","uiComponent","Dholi_MaskBr/js/view/store-mask"],function(c,b,a){return b.extend({defaults:{},initialize:function(){this._super();a.apply()}})});
