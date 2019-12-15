5aa726763ccbe5b579cc97886af5ff2125bc1cc4
eloom <contato@eloom.com.br>
2019-12-14 23:23:49 -0300

    Update cart-mask.js

--- src/module-dholi-mask-br/view/frontend/web/js/view/cart-mask.js
+++ src/module-dholi-mask-br/view/frontend/web/js/view/cart-mask.js
@@ -1,16 +1,1 @@
-define([
-	'uiRegistry',
-	'Dholi_MaskBr/js/view/store-mask',
-], function (registry, mask) {
-	'use strict';
-
-	return function (setShippingInformationAction) {
-		registry.async('checkoutProvider')(function (checkoutProvider) {
-			checkoutProvider.on('shippingAddress', function (data) {
-				mask.apply();
-			});
-		});
-
-		return setShippingInformationAction;
-	}
-});
+define(["uiRegistry","Dholi_MaskBr/js/view/store-mask"],function(b,a){return function(c){b.async("checkoutProvider")(function(d){d.on("shippingAddress",function(e){a.apply()})});return c}});
